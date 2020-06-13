cee92c3e665700e66d9e3c16d216b19de88f26c4
jenniferchen95 <jenniferchen95>
2020-06-13 00:47:53 -0400

    first commit

---
+++ 0001.Two Sum.js
@@ -0,0 +1,41 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+
+//1) Brute Force 
+//Time O(n^2)
+//Space O(1)
+var twoSum = function(nums, target) {
+     for (let i = 0; i < nums.length; i++) {
+         for (let j = i+1; j < nums.length; j++)
+             if (nums[i] + nums[j] === target) return [i,j]
+     }     
+ };
+
+//2)
+//Time O(n)
+//Space O(n)
+var twoSum = function(nums, target) {
+    let hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target-nums[i]
+        if (hash[complement] === undefined) {
+            hash[nums[i]] = i 
+        } else {
+            return [hash[complement], i]
+        }
+    }
+};

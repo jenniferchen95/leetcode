a704245af1aa0bd27a2d0976c463b855379c797c
jenniferchen95 <jenniferchen95@gmail.com>
2020-06-14 12:13:28 -0400

    fixed 3Sum

--- 0015. 3Sum.js
+++ 0015. 3Sum.js
@@ -18,25 +18,30 @@
 //Time O(n^2)
 //Space O(n)
 var threeSum = function(nums) {
-    nums = nums.sort((a,b) => a-b) //O(nlogn)
-    let res = []
+    nums = nums.sort((a,b) => a-b)
 
+    let res = []
+    
     for (let i = 0; i < nums.length-2; i++) {
         if (nums[i] > 0) break;
         if (i > 0 && nums[i] === nums[i-1]) continue;
-
+        
         let j = i + 1
-        let k = nums.length - 1
+        let k = nums.length-1
+
         while (j < k) {
             let sum = nums[i] + nums[j] + nums[k]
             if (sum === 0) {
                 res.push([nums[i], nums[j], nums[k]])
                 while (nums[j] === nums[j+1]) j++;
-                while (nums[k] === nums[k+1]) k--;
+                while (nums[k] === nums[k-1]) k--;
                 j++;
                 k--;
-            } else if (sum < 0) j++
-            else k--
+            } else if (sum < 0) {
+                j++;
+            } else {
+                k--
+            }
         }
     }
     return res

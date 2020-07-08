83d6216e0e046187e112accef5c6c082b1bf1586
jenniferchen95 <jenniferchen95@gmail.com>
2020-07-07 21:45:46 -0400

    valid parentheses

---
+++ 0020. Valid Parentheses.js
@@ -0,0 +1,50 @@
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+
+// Input: "()"
+// Output: true
+// Example 2:
+
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: "(]"
+// Output: false
+// Example 4:
+
+// Input: "([)]"
+// Output: false
+// Example 5:
+
+// Input: "{[]}"
+// Output: true
+
+//Time O(n)
+//Space O(n)
+var isValid = function(s) {
+    
+    let opposite = {
+        ")":"(",
+        "}":"{",
+        "]":"["
+    }
+    
+    let stack = []
+    for (let bracket of s) {
+        if (opposite[bracket] === undefined) {
+            stack.push(bracket)
+        } else {
+            if (opposite[bracket] !== stack.pop()) return false
+        }
+    }
+    
+    return stack.length === 0
+};

8d2321063515d23f5c72513a1bcca5b4a972d721
jenniferchen95 <jenniferchen95@gmail.com>
2020-07-30 13:21:27 -0400

    decode string

---
+++ 0394. Decode String.js
@@ -0,0 +1,48 @@
+// Given an encoded string, return its decoded string.
+
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+// You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+// Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+// Example 1:
+
+// Input: s = "3[a]2[bc]"
+// Output: "aaabcbc"
+// Example 2:
+
+// Input: s = "3[a2[c]]"
+// Output: "accaccacc"
+// Example 3:
+
+// Input: s = "2[abc]3[cd]ef"
+// Output: "abcabccdcdcdef"
+// Example 4:
+
+// Input: s = "abc3[cd]xyz"
+// Output: "abccdcdcdxyz"
+
+var decodeString = function(s) {
+    let currNum = 0
+    let currStr = ""
+    let stack = []
+    
+    for (let c of s) {
+        if (Number.isInteger(Number(c))) {
+            currNum = currNum * 10 + Number(c)
+        } else if (c === "[") {
+            stack.push(currStr)
+            stack.push(currNum)
+            currStr = ""
+            currNum = 0
+        } else if (c === "]") {
+            let num = stack.pop()
+            let prevStr = stack.pop()
+            currStr = prevStr + currStr.repeat(num)
+        } else {
+            currStr += c
+        }
+    }
+    return currStr
+};

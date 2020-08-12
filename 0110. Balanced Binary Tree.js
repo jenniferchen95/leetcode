ab42f91de59e1c5a4fea1f64db181609cf8774dd
jenniferchen95 <jenniferchen95@gmail.com>
2020-08-12 14:34:52 -0400

    updated optimized soln for balanced bt

--- 0110. Balanced Binary Tree.js
+++ 0110. Balanced Binary Tree.js
@@ -28,7 +28,23 @@
 //  4   4
 // Return false.
 
+//1
 //Time O(n)
+//Space O(h)
+var isBalanced = function(root) {
+    return getHeight(root) !== -1
+    
+    function getHeight(root) {
+        if (!root) return 0
+        let left = getHeight(root.left)
+        let right = getHeight(root.right)
+        if (left === -1 || right === -1 || Math.abs(left-right) > 1) return -1
+        return 1 + Math.max(left, right)
+    }
+};
+
+//2) Visits same nodes multiple times 
+//Time O(nlogn)
 //Space O(h)
 var isBalanced = function(root) {
     if (!root) return true

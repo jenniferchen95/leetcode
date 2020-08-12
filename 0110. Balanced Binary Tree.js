0c9019fddc5ee0a415afae14c736990931618e6f
jenniferchen95 <jenniferchen95@gmail.com>
2020-08-12 13:53:36 -0400

    balanced bt

---
+++ 0110. Balanced Binary Tree.js
@@ -0,0 +1,45 @@
+// Given a binary tree, determine if it is height-balanced.
+
+// For this problem, a height-balanced binary tree is defined as:
+
+// a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+// Example 1:
+
+// Given the following tree [3,9,20,null,null,15,7]:
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// Return true.
+
+// Example 2:
+
+// Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+//        1
+//       / \
+//      2   2
+//     / \
+//    3   3
+//   / \
+//  4   4
+// Return false.
+
+//Time O(n)
+//Space O(h)
+var isBalanced = function(root) {
+    if (!root) return true
+    let left = getDepth(root.left)
+    let right = getDepth(root.right)
+    
+    return Math.abs(left-right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+    
+    
+    function getDepth(root) {
+        if (!root) return 0
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1
+    }
+};

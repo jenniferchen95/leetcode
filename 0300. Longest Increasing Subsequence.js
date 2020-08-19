71a302fe9929b9f167c9aff7889b09bd1891d941
jenniferchen95 <jenniferchen95@gmail.com>
2020-08-19 17:18:42 -0400

    optimized soln for #300

--- 0300. Longest Increasing Subsequence.js
+++ 0300. Longest Increasing Subsequence.js
@@ -11,7 +11,30 @@
 // Your algorithm should run in O(n2) complexity.
 // Follow up: Could you improve it to O(n log n) time complexity?
 
-//1) DP
+//1) DP + Binary Search
+//Time O(nlog(n))
+//Space O(n) - dp table 
+var lengthOfLIS = function(nums) {
+    let dp = new Array(nums.length).fill(0)
+    let len = 0
+    
+    for (let num of nums) {
+        let l = 0, r = len-1
+        while (l <= r) {
+            let m = Math.floor(l + (r-l)/2)
+            if (num > dp[m]) {
+                l = m + 1
+            } else {
+                r = m - 1
+            }
+        }
+        dp[l] = num
+        if (l === len) len++
+    }
+    return len
+};
+
+//2) DP
 //Time O(n^2)
 //Space O(n) 
 

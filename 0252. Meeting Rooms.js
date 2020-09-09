31392bc3a99f80a908fa861e42637c0270cdead0
jenniferchen95 <jenniferchen95@gmail.com>
2020-09-09 18:35:25 -0400

    meeting rooms

---
+++ 0252. Meeting Rooms.js
@@ -0,0 +1,10 @@
+//Time O(nlog(n))
+//Space O(1)
+
+var canAttendMeetings = function(intervals) {
+    intervals = intervals.sort((a,b) => a[0]-b[0])
+    for (let i = 0; i < intervals.length-1; i++) {
+        if (intervals[i][1] > intervals[i+1][0]) return false
+    }
+    return true
+};

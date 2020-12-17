be8d2482fb7f3bf45b9a26ea3d4ad66d18293ced
jenniferchen95 <jenniferchen95@gmail.com>
2020-12-16 21:27:33 -0500

    bst preorder traversal

---
+++ 0144. Binary Tree Preorder Traversal.js
@@ -0,0 +1,18 @@
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+
+
+var preorderTraversal = function(root) {
+    if (!root) return []
+    let stack = [root]
+    let res = []
+    
+    while (stack.length) {
+        let curr = stack.pop()
+        res.push(curr.val)
+        if (curr.right) stack.push(curr.right)
+        if (curr.left) stack.push(curr.left)
+    }
+    
+    return res
+};

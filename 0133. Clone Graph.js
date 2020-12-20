5dcda2d2930d6687fdabeb6223bc0ddf4cc8d0c1
jenniferchen95 <jenniferchen95@gmail.com>
2020-12-20 14:05:53 -0500

    clone graph bfs

--- 0133. Clone Graph.js
+++ 0133. Clone Graph.js
@@ -9,6 +9,7 @@
 //     public List<Node> neighbors;
 // }
 
+//1) DFS
 //Time O(N+E)
 //Space O(N)
 var cloneGraph = function(node) {
@@ -27,4 +28,25 @@ var cloneGraph = function(node) {
         
         return newNode
     }
+};
+
+//2) BFS
+var cloneGraph = function(node) {
+    if (!node) return node
+    let map = new Map();
+    let queue = [node]
+    map.set(node, new Node(node.val))
+    
+    while (queue.length) {
+        let curr = queue.shift()
+        
+        for (let neighbor of curr.neighbors) {
+            if (!map.has(neighbor)) {
+                map.set(neighbor, new Node(neighbor.val))
+                queue.push(neighbor)
+            }
+            map.get(curr).neighbors.push(map.get(neighbor))
+        }
+    }
+    return map.get(node)
 };

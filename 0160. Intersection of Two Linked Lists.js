4d8771332660149d20add75308df9507eef7bb54
jenniferchen95 <jenniferchen95@gmail.com>
2020-12-22 18:42:50 -0500

    add soln 160

--- 0160. Intersection of Two Linked Lists.js
+++ 0160. Intersection of Two Linked Lists.js
@@ -6,7 +6,23 @@
 // Output: Reference of the node with value = 8
 // Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
 
-//1) Constant space
+
+//1) Without calculating length
+//Time O(m+n)
+//Space O(1)
+var getIntersectionNode = function(headA, headB) {
+    let currA = headA
+    let currB = headB
+    
+    while (currA !== currB) {
+        currA = currA ? currA.next : headB
+        currB = currB ? currB.next : headA
+    }
+    
+    return currA
+};
+
+//2) Constant space
 //Time O(n+m)
 //Space O(1)
 var getIntersectionNode = function(headA, headB) {
@@ -41,7 +57,7 @@ var getIntersectionNode = function(headA, headB) {
     }
 };
 
-//2) Brute force - Utilizing extra space
+//3) Brute force - Utilizing extra space
 //Time O(m+n)
 //Space O(n or m)
 var getIntersectionNode = function(headA, headB) {

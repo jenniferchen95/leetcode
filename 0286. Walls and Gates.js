f0b4d4427dfb1c28e439d301b2953e27359d7782
jenniferchen95 <jenniferchen95@gmail.com>
2020-12-30 12:41:57 -0500

    walls and gates bfs solution

--- 0286. Walls and Gates.js
+++ 0286. Walls and Gates.js
@@ -1,4 +1,32 @@
+//BFS
+//Time O(m*n)
+//Space O(m*n)
+var wallsAndGates = function(rooms) {
+    let queue = []
+    
+    for (let i = 0; i < rooms.length; i++) {
+        for (let j = 0; j < rooms[0].length; j++) {
+            if (rooms[i][j] === 0) queue.push([i,j])
+        }
+    }
+    
+    let dirs = [[1,0], [-1,0], [0,1], [0,-1]]
+ 
+    while (queue.length) {
+        let curr = queue.shift()
+        for (let dir of dirs) {
+            let r = curr[0] + dir[0]
+            let c = curr[1] + dir[1]
+            if (r < 0 || r >= rooms.length || c < 0 || c >= rooms[0].length || rooms[r][c] !== 2147483647) continue
+            queue.push([r,c])
+            rooms[r][c] = rooms[curr[0]][curr[1]] + 1
+        }
+    }
+    return rooms
+};
 
+
+//DFS
 var wallsAndGates = function(rooms) {
     for (let i = 0; i < rooms.length; i++) {
         for (let j = 0; j < rooms[0].length; j++) {

dc22cdbd64ca909a1cfd466608eebd63f4c5fd5e
jenniferchen95 <jenniferchen95@gmail.com>
2020-12-30 14:06:26 -0500

    del node in bst

---
+++ 0450. Delete Node in a BST.js
@@ -0,0 +1,64 @@
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+// Basically, the deletion can be divided into two stages:
+
+// Search for a node to remove.
+// If the node is found, delete the node.
+// Follow up: Can you solve it with time complexity O(height of tree)?
+
+// Example 1:
+
+// Input: root = [5,3,6,2,4,null,7], key = 3
+// Output: [5,4,6,2,null,null,7]
+// Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+// One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+// Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,7], key = 0
+// Output: [5,3,6,2,4,null,7]
+// Explanation: The tree does not contain a node with value = 0.
+// Example 3:
+
+// Input: root = [], key = 0
+// Output: []
+ 
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -105 <= Node.val <= 105
+// Each node has a unique value.
+// root is a valid binary search tree.
+// -105 <= key <= 105
+
+//Time O(h)
+//Space O(h)
+
+var deleteNode = function(root, key) {
+    if (!root) return null
+    
+    if (root.val > key) {
+        root.left = deleteNode(root.left, key)
+    } else if (root.val < key) {
+        root.right = deleteNode(root.right, key)
+    } else {
+        if (!root.left) {
+            return root.right
+        } else if (!root.right) {
+            return root.left
+        } else {
+            let min = findMin(root.right)
+            root.val = min.val
+            root.right = deleteNode(root.right, root.val)
+        }
+    }
+    return root
+    
+    function findMin(node) {
+        while (node.left) {
+            node = node.left
+        }
+        return node
+    }
+};

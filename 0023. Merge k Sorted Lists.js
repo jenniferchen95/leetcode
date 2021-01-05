98ed88f6f4550723abdc2627e2c4972c18cabfd6
jenniferchen95 <jenniferchen95@gmail.com>
2021-01-05 13:21:51 -0500

    merge k sorted list, optimal solution

--- 0023. Merge k Sorted Lists.js
+++ 0023. Merge k Sorted Lists.js
@@ -10,7 +10,42 @@
 // ]
 // Output: 1->1->2->3->4->4->5->6
 
-//1) Merge lists one by one
+//1) Divide and Conquer
+//Time O(N*logK) K = number of LL
+//We can merge two sorted LL in O(n)
+//Space O(1)
+var mergeKLists = function(lists) {
+    if (!lists.length) return null
+    let interval = 1
+    
+    while (interval < lists.length) {
+        for (let i = 0; i + interval < lists.length; i = i + interval*2) {
+            lists[i] = mergeLists(lists[i], lists[i+interval])
+        }
+        interval *= 2
+    }
+    return lists[0]
+    
+    function mergeLists(l1, l2) {
+        let dummy = new ListNode()
+        let curr = dummy
+        
+        while (l1 && l2) {
+            if (l1.val < l2.val) {
+                curr.next = l1
+                l1 = l1.next
+            } else {
+                curr.next = l2
+                l2 = l2.next
+            }
+            curr = curr.next
+        }
+        curr.next = l1 || l2
+        return dummy.next
+    }
+};
+
+//2) Merge lists one by one
 //Time O(k*N)
 //Space O(1)
 var mergeKLists = function(lists) {
@@ -41,5 +76,3 @@ function mergeLists(l1, l2) {
     curr.next = l1 || l2
     return dummy.next
 }
-
-//2) Min-Heap 

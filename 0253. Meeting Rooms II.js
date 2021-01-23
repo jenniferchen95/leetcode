841c498ca75c975d0e0c37fb6566681b8a4ed4fb
jenniferchen95 <jenniferchen95@gmail.com>
2021-01-23 17:13:48 -0500

    meeting rooms ii alt solution

--- 0253. Meeting Rooms II.js
+++ 0253. Meeting Rooms II.js
@@ -19,3 +19,24 @@ var minMeetingRooms = function(intervals) {
     
     return rooms
 };
+
+// Python with minHeap
+// class Solution:
+//     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+//         if not intervals:
+//             return 0
+        
+//         free_rooms = []
+        
+//         intervals.sort(key = lambda x: x[0])
+        
+//         heapq.heappush(free_rooms, intervals[0][1])
+        
+//         for i in intervals[1:]:
+//             if i[0] < free_rooms[0]: 
+//                 heapq.heappush(free_rooms, i[1])
+//             else: 
+//                 heapq.heappop(free_rooms)
+//                 heapq.heappush(free_rooms, i[1])
+        
+//         return len(free_rooms)
